debcee3063e9df419e685fecf31b659c33233623
soniapop93 <popsonia85@gmail.com>
2023-05-06 19:56:03 +0300

    first commit

---
+++ ServiceAutoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        /*
+           =============================================================
+           =============================================================
+            
+            Service Auto App
+
+            [ ] the person that is on the shift at the auto service should authenticate with a user
+            [ ] the following type of users should have different permissions
+                [ ] Normal user
+                [ ] Admin user -> this user is the only one that can delete/create/update the normal user
+            [ ] possibility to register new customers
+            [ ] the customers can have multiple cars
+            [ ] every car should have a history that contains the parts changed, price, date, car number, color, km, owner, status
+
+           =============================================================
+           =============================================================
+        */
+
+        Console.WriteLine("------------------------ SCRIPT STARTED ------------------------");
+
+       
+
+        Console.WriteLine("------------------------ SCRIPT FINISHED ------------------------");
+    }
+}
---
+++ ServiceAutoApp/User/NormalUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceAutoApp.User
+{
+    public class NormalUser
+    {
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public DateTime registeredDate { get; set; }
+
+        public NormalUser(
+            int userId, 
+            string username, 
+            string password, 
+            string firstName, 
+            string lastName, 
+            string email, 
+            string phone, 
+            string address, 
+            DateTime registeredDate)
+        {
+            this.userId = userId;
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phone = phone;
+            this.address = address;
+            this.registeredDate = registeredDate;
+        }
+    }
+}

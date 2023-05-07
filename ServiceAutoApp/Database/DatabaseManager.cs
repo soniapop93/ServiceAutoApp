a6f94a4d00178faf7995a99f8b036559f8dc3084
soniapop93 <popsonia85@gmail.com>
2023-05-07 21:57:40 +0300

    started to implement the database part

---
+++ ServiceAutoApp/Database/DatabaseManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SQLite;
+
+namespace ServiceAutoApp.Database
+{
+    public class DatabaseManager
+    {
+        private SQLiteConnection sqLiteConnection;
+
+        public DatabaseManager()
+        {
+            generateDB("C:\\Users\\" + System.Environment.UserName + "\\Desktop", "ServiceAutoDB");
+        }
+        private void generateDB(string path, string fileName)
+        {
+            string fileNamePath = path + @"\" + fileName + ".db";
+            createConnection(fileNamePath);
+
+            Console.WriteLine("Database is created successfully");
+            createTableUsers();
+        }
+
+        private void createConnection(string fileNamePath)
+        {
+            string strConnection = String.Format("Data Source={0};Version=3;New=True;Compress=True;", fileNamePath);
+            sqLiteConnection = new SQLiteConnection(strConnection);
+        }
+
+        public void createTableUsers()
+        {
+            string strSql = "CREATE TABLE IF NOT EXISTS Users " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "username TEXT, " +
+                "password TEXT, " +
+                "firstName TEXT, " +
+                "lastName TEXT, " +
+                "email TEXT, " +
+                "phone TEXT, " +
+                "address TEXT, " +
+                "registredDate TEXT, " +
+                "admin TEXT);";
+
+            sqLiteConnection.Open();
+            SQLiteCommand sqLiteCommand = sqLiteConnection.CreateCommand();
+            sqLiteCommand.CommandText = strSql;
+            sqLiteCommand.ExecuteNonQuery();
+            sqLiteConnection.Close();
+        }
+
+        public void createTableCustomers()
+        {
+
+        }
+    }
+}
